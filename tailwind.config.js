30e9d29597bd764511189a5e1b50c013f4803d78
amarjeet <amarjeetrao33@gmail.com>
2019-07-18 14:23:39 +0530

    full setup complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        'body': ['Montserrat', '-apple-system, BlinkMacSystemFont', 'Roboto', 'Helvetica Neue', 'Arial'],
-        'heading': ['Abril Fatface', 'Cambria', 'Times New Roman', 'Times', 'serif']
-      },
-      color: {
-        'cyan-lite': '#00FFCC'
+    theme: {
+      extend: {
+        fontFamily: {
+          'body': ['Montserrat', '-apple-system, BlinkMacSystemFont', 'Roboto', 'Helvetica Neue', 'Arial'],
+          'heading': ['Abril Fatface', 'Cambria', 'Times New Roman', 'Times', 'serif']
+        },
+        colors: {
+          cyan: {
+            lite: '#00FFCC'
+        }
       },
       borderWidth: {
         '12': '12px'
