faecfe7a3704007bc10a7162b1b6a5929dc46cc7
amarjeet <amarjeetrao33@gmail.com>
2019-07-21 14:50:39 +0530

    Mobile responsive stage complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,22 @@ module.exports = {
           'body': ['Montserrat', '-apple-system, BlinkMacSystemFont', 'Roboto', 'Helvetica Neue', 'Arial'],
           'heading': ['Abril Fatface', 'Cambria', 'Times New Roman', 'Times', 'serif']
         },
+        fontSize: {
+          '8xl': '6rem',
+          '10xl': '8rem',
+          '12xl': '10rem',
+          '14xl': '12rem',
+          '15xl': '14rem',
+          '16xl': '16rem'
+        },
         colors: {
           cyan: {
             lite: '#00FFCC'
         },
           'custom-gray': '#686868',
+      },
+      opacity: {
+        '10': '0.1'
       },
       borderWidth: {
         '14': '14px',
@@ -20,6 +31,8 @@ module.exports = {
       },
     }
   },
-  variants: {},
+  variants: {
+    height:['responsive']
+  },
   plugins: []
 }
